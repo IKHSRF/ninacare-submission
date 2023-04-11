529a43f20a18771eea158e98f2a5947d20d542ec
ikhsrf <ikhsan3568@gmail.com>
2023-04-11 12:55:43 +0700

    [CREATE] create user controller

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,10 +7,32 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    public function filter(Request $request) {
+    public function filter(Request $request)
+    {
         try {
-            $data = UserModel::wh
-        } catch(\Exception $e) {
+            $data = UserModel::with('language', 'religion')
+                ->join('childrens', 'childrens.user_id', 'users.id')
+                ->where(function ($query) use ($request) {
+                    if ($request->filter['name']) {
+                        $query->where('users.name', $request->filter['name']);
+                    }
+                    if ($request->filter['age'] && filter_var($request->filter['age'], FILTER_VALIDATE_INT) === true) {
+                        $query->where('users.age', $request->filter['age']);
+                    }
+                    if ($request->filter['gender'] && filter_var($request->filter['gender'], FILTER_VALIDATE_INT) === true) {
+                        $query->where('users.gender', $request->filter['gender']);
+                    }
+                    if ($request->filter['location']) {
+                        $query->where('users.location', $request->filter['location']);
+                    }
+                })
+                ->paginate(15);
+
+
+            return response()->json([
+                'users' => $data,
+            ]);
+        } catch (\Exception $e) {
             return $e;
         }
     }
